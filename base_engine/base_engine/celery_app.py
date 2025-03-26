202bda0f37fb36ecb3e559719559529998319bc2
= <yohan.gastoud@patrowl.io>
2025-03-26 22:42:16 +0100

    feat: celery docker ✨

--- base_engine/base_engine/celery_app.py
+++ base_engine/base_engine/celery_app.py
@@ -5,3 +5,4 @@ from celery import Celery
 RABBITMQ_ADDRESS = os.environ.get("RABBITMQ_ADDRESS", "localhost")
 
 app = Celery("scans", broker=f"pyamqp://guest:guest@{RABBITMQ_ADDRESS}//")
+app.conf.broker_connection_retry_on_startup = True
